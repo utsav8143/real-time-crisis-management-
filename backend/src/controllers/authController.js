31d75727675f95b3e38e6a1dbf69e3f832eeeb64
utsav8143 <utsav8143@gmail.com>
2026-07-18 01:46:19 +0530

    completed auth controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import config from "../config/config.js";
 import sessionmodel from "../models/Sessionmodel.js";
-import { decode } from "punycode";
+
 
 // @desc Register User
 // @ROUTE POST api/auth/register
@@ -148,7 +148,9 @@ export async function login(req, res) {
     });
   } catch (err) {
     console.log("Error in fething the user:", err);
-    return res.status(500).json({message:"Server Error", error: err.message});
+    return res
+      .status(500)
+      .json({ message: "Server Error", error: err.message });
   }
 }
 
@@ -190,7 +192,7 @@ export async function getMe(req, res) {
 
 export async function refreshToken(req, res) {
   const refreshToken = req.cookies.refreshToken;
-  console.log(refreshToken)
+  
   try {
     if (!refreshToken) {
       return res.status(401).json({ message: "Refresh token not found" });
@@ -259,9 +261,33 @@ export async function refreshToken(req, res) {
 // @desc Logout User
 // @ROUTE GET /api/auth/logout
 
-export async function logout(req, res) {}
+export async function logout(req, res) {
+  const refreshToken = req.cookies.refreshToken;
 
-// @desc Verify Email
-// @ROUTE POST /api/auth/verify-email
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh Token not found" });
+  }
 
-export async function verifyEmail(req, res) {}
+  const refreshTokenHashed = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  const session = await sessionmodel.findOne({
+    refreshTokenHashed,
+    revoked: false,
+  });
+
+  if (!session) {
+    return res.status(401).json({ message: "Invalid refresh Token" });
+  }
+
+  session.revoked = true;
+  await session.save();
+
+  res.clearCookie("refreshToken");
+
+  res.status(200).json({ message: "Logged out successfully" });
+}
+
+
