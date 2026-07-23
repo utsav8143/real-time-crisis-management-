4f62a62f9c2aa9cd7a3cfba96e5aa2d718b287c3
utsav8143 <utsav8143@gmail.com>
2026-07-23 18:58:58 +0530

    create incident

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -1,29 +1,50 @@
- import incidentModel from "../models/incidentModel.js";
-
+import incidentModel from "../models/incidentModel.js";
 
 //  @desc report incident
 // @ROUTE POST/api/incident/report
- export async function createIncident(req,res) {
-    
- }
+export async function createIncident(req, res) {
+  const { title, description, category, location, reportedBy } = req.body;
 
+  try {
+    if (!coordinates || coordinates.length !== 2) {
+      return res
+        .status(400)
+        .json({ message: "Valid location coordinates are required" });
+    }
+
+    const incident = await incidentModel.create({
+      title,
+      description,
+      category,
+      location: {
+        type: "Point",
+        coordinates, //[lng,lat]
+        address,
+      },
+      reportedBy: req.user._id,
+    });
+
+    const populate = await incidentModel.populate(
+      "reportedBy",
+      "name email role",
+    );
+
+    res
+      .status(201)
+      .json({ message: "Incident created successfully", populate });
+  } catch (err) {
+    res.status(500).json({ message: "Error in creating incident:", err });
+  }
+}
 
 //  @desc view Incidents
 // @ROUTE GET/api/incident/view-incidents
- export async function viewIncidents(req,res) {
-    
- }
-
+export async function viewIncidents(req, res) {}
 
 //  @desc view Incidents by ID
 // @ROUTE GET/api/incident/view-incidents/:id
- export async function viewIncidentsById(req,res) {
-    
- }
+export async function viewIncidentsById(req, res) {}
 
- 
 //  @desc update Incidents
 // @ROUTE GET/api/incident/update-incidents
- export async function updateIncident(req,res) {
-    
- }
+export async function updateIncident(req, res) {}
