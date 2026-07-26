5b671b1c7046f4331f4d7516ad484b4c6fc0b632
utsav8143 <utsav8143@gmail.com>
2026-07-27 01:15:42 +0530

    Completed axiosInstance and updated gitignore

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api=axios.create({
+    baseURL:import.meta.env.VITE_API_URL || "http://localhost:5000"
+})
+
+// @ Request Interceptor
+// Attach token to every outgoing request
+api.interceptors.request.use((config)=>{
+    const token=localStorage.getItem("token")
+    if(token){
+        config.headers.Authorization= `Bearer ${token}`
+    }
+    return config;
+});
+
+// @Response interceptor
+// Auto-logout if token expires 
+api.interceptor.response.use(
+    (response) => response,
+    (error) => {
+        if(error.response?.status===401){
+            console.log("Unauthorized user")
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href="/login";
+        }
+
+        return Promise.reject(error);
+    }
+)
+
+export default api;
