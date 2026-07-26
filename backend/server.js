c3de83135b00b5a23d1d5cbcf3bb2a5bc3e115e8
utsav8143 <utsav8143@gmail.com>
2026-07-26 18:37:23 +0530

    socket.io setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import app from "./src/app.js";
 import express from "express";
 import connectDB from "./src/config/dbConfig.js";
+import { Server } from "socket.io";
+import {createServer} from "http"
 
 connectDB();
+
+
+const server=createServer(app)
+const io=new Server(server)
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
+
+
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${port}`);
 });
