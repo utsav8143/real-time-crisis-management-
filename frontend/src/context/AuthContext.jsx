23ff6a47fc47084a90be6c435b9476307684484e
utsav8143 <utsav8143@gmail.com>
2026-07-27 04:38:10 +0530

    completed AuthContext

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import api from "../utils/axiosInstance";
+
+const AuthContext = createContext(null);
+
+export function authProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+
+    if (storedUser && token) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = async (email, password) => {
+    const { data } = await api.post("/auth/login", { email, passowrd });
+    const { token, ...userData } = data;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+    return userData;
+  };
+
+  const register = async (name, email, password, role) => {
+    const { data } = await api.post("/auth/register", {
+      name,
+      email,
+      password,
+      role,
+    });
+    const { token, ...userData } = data;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", userData);
+    setUser(userData);
+    return userData;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// Custom hook so componnets just do: const{user, login}=useAuth()
+export function useAuth() {
+  const ctk = useContext(AuthContext);
+  if (!ctk) {
+    throw new Error("useAuth must be used within AuthProvider ");
+  }
+  return ctk;
+}
