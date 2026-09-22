e9e0f74dd36718dc547570a28921c1d81520647e
utsav8143 <utsav8143@gmail.com>
2026-09-23 01:58:28 +0530

    change in backend url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:import.meta.env.VITE_API_URL || "https://resq-5lul.onrender.com",
+    baseURL:import.meta.env.VITE_API_URL || "https://resq-5s4q.onrender.com",
     withCredentials:true,
 })
 
